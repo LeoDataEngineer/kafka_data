ab9b7f90c3b70f364dfe198751687ee00327ff89
LeoDataEngineer <116231124+LeoDataEngineer@users.noreply.github.com>
2024-04-16 20:57:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from datetime import timezone
-from random import  randint
+from random import randint
 from confluent_kafka import Producer
 from faker import Faker
 import pandas as pd
-
+import socket
 
 # instanciar un objeto Faker para generar datos ficticios
 fake = Faker()
@@ -15,7 +15,8 @@ kafka_config = {
     'sasl.mechanism': 'SCRAM-SHA-256',
     'security.protocol': 'SASL_SSL',
     'sasl.username': 'ZGFzaGluZy1pYmV4LTY4MDYkFoaV9RQtF1nv3j4u15zZ7mY9RJStxIlXkKOyIyE',
-    'sasl.password': 'MjM4YjZhYTktMzc1MC00MzJhLWEyNTAtMjVhOWUwMzM5OGY2'
+    'sasl.password': 'MjM4YjZhYTktMzc1MC00MzJhLWEyNTAtMjVhOWUwMzM5OGY2',
+    'client.id': socket.gethostname()
 }
 
 producer = Producer(kafka_config)
@@ -45,29 +46,35 @@ def delivery_callback(err, msg):
         print(f'Message failed delivery: {err}')
     else:
         print(f'Message delivered to {msg.topic()}: {msg.value()}')
-        
+
 for i in range(randint(1, 5)):
     transaction_record = generate_fake_transaction(fake)
     cc_no = transaction_record["cc_no"]
-    cc_provider =  transaction_record["cc_provider"]
+    cc_provider = transaction_record["cc_provider"]
     cc_expire = transaction_record["cc_expire"]
     amount = transaction_record["amount"]
     currency = transaction_record["currency"]
     merchant = transaction_record["merchant"]
     timestamp = transaction_record["timestamp"]
-    
+
     # Convertir a formato de fecha y hora
     timestamp = pd.to_datetime(timestamp)
 
     # Formatear como deseas
     timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
 
-
-    transaction_record =f"{cc_no} - {cc_provider} - {cc_expire} - {amount} - {currency} - {merchant} - {timestamp}"
+    transaction_record = f"{cc_no} - {cc_provider} - {cc_expire} - {amount} - {currency} - {merchant} - {timestamp}"
 
     producer.produce(
         'transaccion_tc',
         transaction_record.encode('utf-8'),
         callback=delivery_callback
     )
-    producer.poll(0)
+
+# Espera a que todos los mensajes sean entregados
+producer.flush()
+
+
+
+        
+
